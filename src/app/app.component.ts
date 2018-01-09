a04607529c13c039e7c3305d0ad2a33e10abd31b
TPrasadGit <tirupati.prasad@hotmail.com>
2018-01-09 07:29:55 +0530

    Initial Commit
    Committing my first changes to GitHub

---
+++ src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+declare var jQuery:any;
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent implements OnInit {
+  pageTitle :string = 'Getting Started';
+  showSideNav : boolean = true;
+
+  ngOnInit(){
+    jQuery('button.side-navbar-btn').click(function () {
+      var isLeftNegative = jQuery(this).parent().hasClass('slide-left');
+      if (isLeftNegative) {
+          jQuery(this).parent().css('left', '0');
+          jQuery('div.content-body').css('margin-left', '150px');
+          jQuery(this).parent().removeClass('slide-left');
+      }
+      else {
+          jQuery(this).parent().css('left', '-150px');
+          jQuery('div.content-body').css('margin-left', '0px');
+          jQuery(this).parent().addClass('slide-left');
+      }        
+  });
+  jQuery('button.side-navbar-btn').click();
+  }
+}
